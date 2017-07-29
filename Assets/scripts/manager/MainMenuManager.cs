c7a69ff39c2b84a4db80059e39dbae981a6f3916
abe <abe_Vos@msn.com>
2017-07-29 13:07:50 +0200

    Add basic game loop structure

---
+++ Assets/scripts/manager/MainMenuManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    protected void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+        else if (Input.GetMouseButtonDown(0) || Input.anyKeyDown)
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
