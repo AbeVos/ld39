8243a2b70dc1d6bf1ff310c6dc3226fb2a8431b8
abe <abe_Vos@msn.com>
2017-07-29 14:01:15 +0200

    Add basic security cam behaviour

--- Assets/scripts/manager/GameManager.cs
+++ Assets/scripts/manager/GameManager.cs
@@ -1,16 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    public static void EndGame()
+    {
+        SceneManager.LoadScene(0);
+    }
 }
