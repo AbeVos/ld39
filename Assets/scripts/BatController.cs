33980780b2ed20a18a6790c5afba2fa3871bbf6c
Mathijs Lemmers <mathijs750@gmail.com>
2017-07-31 23:12:44 +0200

    UI preties

---
+++ Assets/scripts/BatController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BatController : MonoBehaviour
+{
+    [SerializeField]
+    private Player player;
+
+    private Image[] bars;
+    private float barScale;
+
+	// Use this for initialization
+    private void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
